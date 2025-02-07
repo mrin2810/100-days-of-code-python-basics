eefef156cea94b12ada60b64435a02eb2acc5e89
Mrinmayee Rane <mrinmayeerane2810@gmail.com>
2025-02-06 19:56:11 -0500

    Update rock-paper-scissors.py

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,8 +1,39 @@
 import random
 
+graphics = [
+  """
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+""",
+  """
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+""",
+  """
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""
+]
+
 player_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper and 2 for Scissors."))
+print("You Chose:")
+print(graphics[player_choice])
 
 computer_choice = random.randin(0, 2)
+print("Computer Chose:")
+print(graphics[computer_choice])
 
 if player_choice == computer_choice:
   print("It's a Tie")
