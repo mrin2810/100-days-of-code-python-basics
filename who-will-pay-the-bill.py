a7ff01f25ba2d6fb02b84db93001a6a3984d8608
Mrinmayee Rane <mrinmayeerane2810@gmail.com>
2025-02-06 19:41:05 -0500

    Create who-will-pay-the-bill.py

---
+++ who-will-pay-the-bill.py
@@ -0,0 +1,7 @@
+import random
+
+friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+who_will_pay = random.randint(0, len(friends) - 1)
+
+print('Today ' + friends[who_will_pay] + ' will pay!'}
