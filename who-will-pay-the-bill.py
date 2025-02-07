be2fb96cadbd3b4d6718ba79d0d063f2ec4be86f
Mrinmayee Rane <mrinmayeerane2810@gmail.com>
2025-02-06 19:42:53 -0500

    Update who-will-pay-the-bill.py

--- who-will-pay-the-bill.py
+++ who-will-pay-the-bill.py
@@ -4,4 +4,4 @@ friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
 
 # who_will_pay = random.randint(0, len(friends) - 1)
 
-print('Today ' + friends[who_will_pay] + ' will pay!'}
+print('Today ' + random.choice(friends) + ' will pay!'}
